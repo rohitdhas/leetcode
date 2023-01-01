4b03d440f96d4550498b276400b61dc4f5450d63
Rohit Dhas <rohitdhas666@gmail.com>
2023-01-01 15:42:05 +0530

    update lt-ez #872 solution

--- easy/leaf-similer-trees.ts
+++ easy/leaf-similer-trees.ts
@@ -5,6 +5,17 @@
 // Time Complexity: O(T1 + T2), where T1,T2 are the lengths of the given trees.
 // Space Complexity: O(T1 + T2), the space used in storing the leaf values.
 
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {
   const first: number[] = [];
   const second: number[] = [];
